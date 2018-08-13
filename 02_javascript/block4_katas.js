81d3e215b6bd9d65453ffa4a38bb5d9bc4d7a16a
Antonello <antonello@GKs-iMac.local>
2018-08-13 12:55:48 +0200

    adding katas

---
+++ 02_javascript/block4_katas.js
@@ -0,0 +1,7 @@
+ex1 https://www.codewars.com/kata/sum-of-multiples/train/javascript
+
+ex2 https://www.codewars.com/kata/jennys-secret-message/train/javascript
+
+ex3 https://www.codewars.com/kata/plural/train/javascript
+
+ex4 https://www.codewars.com/kata/leonardo-dicaprio-and-oscars/train/javascript
