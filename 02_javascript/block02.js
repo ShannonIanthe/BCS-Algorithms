ba221a0fd9dd83e075c078f980a9751c525a9164
Antonello <antonello@barcelonacodeschool.com>
2018-08-13 15:14:30 +0000

    Update block02.js

--- 02_javascript/block02.js
+++ 02_javascript/block02.js
@@ -225,7 +225,7 @@ var arr = ['green','red','black','blue','brown','yellow','purple']
 
 EXERCISE 13:
 //write a function called characterRemover which takes two arguments, a string and a character to be removed and returns the string free of the unwanted character.
-// please note: in order for thr string to make sense you should replace the unwanted characters with a space so that your string will look like this:
+// please note: in order for the string to make sense you should replace the unwanted characters with a space so that your string will look like this:
 // I really Like Pizza
 
 var str = 'I,Really,Like,Pizza'
