970056e1f13f61e579c25ab558f445bce988bf7b
Antonello <antonello@GKs-iMac.local>
2018-08-13 15:16:36 +0200

    more katas

--- 02_javascript/block6_katas.js
+++ 02_javascript/block6_katas.js
@@ -1,3 +1,7 @@
 ex1 https://www.codewars.com/kata/the-if-function/train/javascript
 
 ex2 https://www.codewars.com/kata/day-of-the-week/train/javascript
+
+ex3 https://www.codewars.com/kata/maximum-product/train/javascript
+
+ex4 https://www.codewars.com/kata/whos-online/train/javascript
