a0bb97368caca960e0189928fa3f7746aa7c0b93
gk3000 <mail@gk3000.com>
2018-09-11 19:25:03 +0200

    b6e10 desc updated

--- 02_javascript/block06.js
+++ 02_javascript/block06.js
@@ -168,7 +168,9 @@ EXERCISE 10
 
 // Create a function called model. The model function allows you to control how you interact with a collection of objects that we call collection that are stored in the DB. Model imposes conditions on the properties of the collection, for example which keys are allowed.
 
-// In this exercise create a model function. This model uses the definitions of a predefined object called schema (you don't need to define it) to assert the keys that each object in a collection that are allowed. The model function can take 2 arguments: the first is the type of the operation we want to execute on the db and the second is the data we need to execute it. For now we only define an operation called 'add'.
+// In this exercise create a model function. This model uses the definitions of a predefined object called schema (you don't need to define it) to assert the keys that each object in a collection that are allowed. The model function can take 3 arguments: the first is the type of the operation we want to execute on the db and the second is the data we need to execute it, and the third one would be our schema object defining the pattern or template for what data is valid for adding to the DB. 
+
+//For now we only define an operation called 'add'.
 
 // if the argument add is not present then nothing should be added.
 
