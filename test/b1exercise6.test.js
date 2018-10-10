4a79f93044f6251c398b4002b96d11fba80ea570
Antonello <antonello@barcelonacodeschool.com>
2018-10-10 14:48:54 +0000

    Update b1exercise6.test.js

--- test/b1exercise6.test.js
+++ test/b1exercise6.test.js
@@ -1,13 +1,7 @@
 var assert       = require('chai').assert
 var compare      = require('../02_javascript/block1/exercise6').compare
-var arr          = require('../02_javascript/block1/exercise6').arr
 
 
-describe("#test1", function(){
-    it ('a should be an array', function(){
-        assert.typeOf(arr, 'array')
-    })
-})
 describe("#test2", function(){
     it ('a should be a function', function(){
         assert.typeOf(compare, 'function')
@@ -15,21 +9,11 @@ describe("#test2", function(){
 })
 describe("#test3", function(){
     it ('compare should be 1', function(){
-        assert.equal(compare([10, 22,43,54,65,34,10,10]),1)
+        assert.equal(compare(10,30), false)
     })
 })
 describe("#test4", function(){
     it ('compare should be 0', function(){
-        assert.equal(compare([10, 44,43,0,2,34,11,12]),0)
-    })
-})
-describe("#test5", function(){
-    it ('compare should be 2', function(){
-        assert.equal(compare([43,66,11,55,3,53,54,65,3,54,64,5,4,6,22,22,17,12,5,5]),2)
-    })
-})
-describe("#test6", function(){
-    it ('compare should be 3', function(){
-        assert.equal(compare([12,12,"Hello", "Hello", {},{},[],[], 'ciao','ciao']),3)
+        assert.equal(compare(10,10),true)
     })
 })
