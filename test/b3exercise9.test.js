0886a9b84f831d74948a02264ae2c40773fcc148
gk3000 <mail@gk3000.com>
2018-10-18 10:18:10 +0200

    minor change text b3e9

--- test/b3exercise9.test.js
+++ test/b3exercise9.test.js
@@ -7,22 +7,22 @@ describe("#test1", function(){
     })
 })
 describe("#test2", function(){
-    it ('i should be 2', function(){
+    it ('count should be 2', function(){
         assert.equal(twoArrays([10,10,10], [10,10,5]), 2)
     })
 })
 describe("#test3", function(){
-    it ('i should be 2', function(){
+    it ('count should be 2', function(){
         assert.equal(twoArrays([1,5,80], [10,5,80]), 2)
     })
 })
 describe("#test2", function(){
-    it ('i should be 0', function(){
+    it ('count should be 0', function(){
         assert.equal(twoArrays([3,5], [1, 4,5]), 0)
     })
 })
 describe("#test2", function(){
-    it ('i should be 0', function(){
+    it ('count should be 0', function(){
         assert.equal(twoArrays([3,5], ['3','5']), 0)
     })
 })
