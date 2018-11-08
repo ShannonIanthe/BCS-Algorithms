7ed0f94c1e9844a204eb35a6bb49b02fff34b1a7
Antonello <antonello@barcelonacodeschool.com>
2018-11-08 16:46:56 +0000

    Update b8exercise2.test.js

--- test/b8exercise2.test.js
+++ test/b8exercise2.test.js
@@ -9,13 +9,8 @@ describe("#test1", function(){
 })
 describe("#test2", function(){
     it ('recursive should be robert, 22,roger,44', function(){
-        var a = []
-        var arr = recursive([{name:'robert', age:22},{name:'robert', age:12},{name:'roger', age:44},{name:'peter', age:98},{name:'Ralph', age:67}],0,a)
-        assert.equal(a[0][0], 'robert')
-        assert.equal(a[0][1], 22)
-        assert.equal(a[2][0], 'roger')
-        assert.equal(a[2][1], 44)
-
-
+       assert.deepEqual(recursive([{name:'robert', age:22},{name:'robert', age:12},{name:'roger', age:44},{name:'peter', age:98},{name:'Ralph', age:67}]),[
+        ['robert', 22],['robert', 12],['roger', 44],['peter', 98],['Ralph', 67] 
+       ])
     })
 })
