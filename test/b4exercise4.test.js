8017f01a8c4fb34f9f6d4c07b58b91fc9ae36fac
gk3000 <george@barcelonacodeschool.com>
2019-01-10 11:22:02 +0000

    Update b4exercise4.test.js

--- test/b4exercise4.test.js
+++ test/b4exercise4.test.js
@@ -9,18 +9,18 @@ describe("#test0", function(){
 describe("#test1", function(){
     it ('checker should be 1 comma 2 question marks', function(){
         assert.equal(checker("hello, how are you today? I am not bad and you?"),
-        "1 comma 2 question marks")
+        "1 comma, 2 question marks")
     })
 })
 describe("#test2", function(){
     it ('checker should be 2 commas 2 question marks', function(){
         assert.equal(checker("hello, how are you today? I am not bad and you?, i feel great"),
-        "2 commas 2 question marks")
+        "2 commas, 2 question marks")
     })
 })
 describe("#test3", function(){
     it ('checker should be 0 comma 1 question mark', function(){
         assert.equal(checker("do you line banana?"),
-        "0 comma 1 question mark")
+        "0 comma, 1 question mark")
     })
 })
