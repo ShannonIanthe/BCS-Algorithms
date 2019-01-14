b13897d9883930f11d96f18912a221cb6a13e28f
gk3000 <mail@gk3000.com>
2019-01-14 09:47:03 +0100

    jan 14 before pull

--- 02_javascript/block3/exercise13.js
+++ 02_javascript/block3/exercise13.js
@@ -1,3 +1,11 @@
+var shortener = function (str) {
+var newStr = str.split(' ');
+newStr[1] = newStr[1][0].toUpperCase() + '.';
+newStr[0][0] = newStr[0][0].toUpperCase()
+return newStr.join(' ')
+
+}
+
 module.exports = {
     shortener
 }
--- 02_javascript/block4/exercise8.js
+++ 02_javascript/block4/exercise8.js
@@ -1,4 +1,17 @@
+var arr = ['mike','jason','peter','robert','mike','jason','jenny','jane']
 
+
+var uniqueElements = function(arr) {
+var arr2 = [];
+for(var i = 0; i < arr.length; i++) {
+if (!arr2.includes(arr[i])) {
+arr2.push(arr[i])
+   }
+     }
+return `old array ${arr}\n new array ${arr2}`
+
+   }
+   
 module.exports = {
     uniqueElements
 }
