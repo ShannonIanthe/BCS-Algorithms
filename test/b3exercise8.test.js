f2a10552c941eeeabcc2634223173a2e130156ec
GK <gk@GKs-iMac.local>
2019-08-21 12:00:38 +0200

    removed strings for the array results

--- test/b3exercise8.test.js
+++ test/b3exercise8.test.js
@@ -8,16 +8,16 @@ describe("#test1", function(){
 })
 describe("#test2", function(){
     it ('i should be [20,20,20]', function(){
-        assert.equal(timesTwo([10,10,10]), [20,20,20])
+        assert.deepEqual(timesTwo([10,10,10]), [20,20,20])
     })
 })
 describe("#test3", function(){
     it ('i should be [10,8,4]', function(){
-        assert.equal(timesTwo([5, 4,2]), [10,8,4])
+        assert.deepEqual(timesTwo([5, 4,2]), [10,8,4])
     })
 })
 describe("#test4", function(){
     it ('i should be  [30,8,4]', function(){
-        assert.equal(timesTwo([15, 4,2]), [30,8,4])
+        assert.deepEqual(timesTwo([15, 4,2]), [30,8,4])
     })
 })
