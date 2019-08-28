5f495437c863a5429152cb9e9e141bdc123a2c37
gk3000 <mail@gk3000.com>
2019-08-28 15:41:14 +0200

    b6e15 switched to deepEqual

--- test/b6exercise15.test.js
+++ test/b6exercise15.test.js
@@ -6,23 +6,17 @@ describe("#test1", function(){
         assert.typeOf(sort, 'function')
     })
 })
+
 describe("#test2", function(){
-    it ('sort should be [1,1,3,4,4,20]', function(){
+    it ('sort should be {a: 1, e: 1, c: 3, d: 4, f: 4, b: 20}', function(){
         var obj = sort({a: 1, b: 20, c: 3, d: 4, e: 1, f: 4})
-        assert.equal(String(Object.values(obj)), String([1,1,3,4,4,20]))
+        assert.deepEqual(sort({a: 1, b: 20, c: 3, d: 4, e: 1, f: 4}), {a: 1, e: 1, c: 3, d: 4, f: 4, b: 20})
     })
 })
+
 describe("#test3", function(){
-    it ("sort should be a ['a','e','c','d','f','b']", function(){
-        var obj = sort({a: 1, b: 20, c: 3, d: 4, e: 1, f: 4})
-        assert.equal(String(Object.values(obj)), String([1,1,3,4,4,20]))
-        assert.equal(String(Object.keys(obj)), String(['a','e','c','d','f','b']))
-    })
-})
-describe("#test4", function(){
-    it ("sort should be ['b','age','a','year']", function(){
+    it ("sort should be {b: 3, age: 22, a: 99, year: 1999}", function(){
         var obj = sort({age:22, year:1999, a:99, b:3})
-        assert.equal(String(Object.values(obj)), String([3,22,99,1999]))
-        assert.equal(String(Object.keys(obj)), String(['b','age','a','year']))
+        assert.deepEqual(sort({age:22, year:1999, a:99, b:3}), {b: 3, age: 22, a: 99, year: 1999})
     })
 })
