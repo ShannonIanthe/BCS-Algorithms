051def1003b7209c04f00cb9be5be076f6a1dd4b
estefanodischool <stefano@barcelonacodeschool.com>
2020-03-23 17:31:54 +0100

    updated b1 descriptions

--- 02_javascript/block01.js
+++ 02_javascript/block01.js
@@ -22,156 +22,170 @@
 
 EXERCISE 1:
 
-//create a variable called apple and assign to it a value of 5
-//then create another variable called apple2 and give to it a value of 15.
-//and finally sum the together and return their sum.
+// create a variable called apple and assign to it a value of 5
+// then create another variable called apple2 and give to it a value of 15.
+// and finally sum the together and return their sum.
 
-
-
-
-
+//======================  EXAMPLE  ========================
+// total(15,5)
+// 20
+// total(20,20)
+// 40
+//=========================================================
 
 
 EXERCISE 2:
 
-//create 2 variables:
-//a , b
-//assign 10 to a and 24 to b
+// create 2 variables:
+// a , b
+// assign 10 to a and 24 to b
+// return the total of the multiplication of a and b
 
-//return the total of the multiplication of a and b
-
-
-
-
-
-
+//======================  EXAMPLE  ========================
+// multy(10,5)
+// 50
+// multy(20,20)
+// 400
+//=========================================================
 
 
 EXERCISE 3:
 
-//Age calculator
+// Age calculator
+// Want to find out how old you'll be? Calculate it!
+// Store your birth year in a variable.
+// Store a future year in a variable.
+// Output the age!
 
-//Want to find out how old you'll be? Calculate it!
-
-//Store your birth year in a variable.
-
-//Store a future year in a variable.
-
-
-//Output the age!
-
-
-
-
-
-
+//======================  EXAMPLE  ========================
+// ageCalc(1982,2018)
+// 36
+// ageCalc(2000,2018)
+// 18
+//=========================================================
 
 
 EXERCISE 4:
 
-//Ever wondered how much a "lifetime supply of Tea you will need?"
-//let's find out!!!
-//store  your current age in a variable
-//store your estimated end age in a variable
-//store how many teas you drink per day on average
-//calculate how many you will need until the end!
+// Ever wondered how much a "lifetime supply of Tea you will need?"
+// let's find out!!!
+// store  your current age in a variable
+// store your estimated end age in a variable
+// store how many teas you drink per day on average
+// calculate how many you will need until the end!
+// Output the result!
 
-//Output the result!
+//======================  EXAMPLE  ========================
+// howManyTeas(35,70, 2)
+// 25550
+// howManyTeas(35,36, 2)
+// 730
+//=========================================================
 
 
 EXERCISE 5:
 
-//Define a function called compare which takes 2 arguments, and returns true if the first is bigger than the second and false if is not.
-// example 
-//compare(10, 34)
-// false
-// compare(100, 34)
+// Define a function called compare which takes 2 arguments, and returns true if the first is bigger than the second and false if is not.
+
+//======================  EXAMPLE  ========================
+// compare(100, 50)
 // true
+// compare(1, 100)
+// false
+//=========================================================
 
 EXERCISE 6:
 
-//Define a function called compare which takes 2 arguments, and returns true if the first is equal to the second and false if is not (strict equality).
+// Define a function called compare which takes 2 arguments, and returns true if the first is equal to the second and false 
+// if is not (strict equality).
 
-// example 
-//compare(10, 34)
+//======================  EXAMPLE  ========================
+// compare(10, 34)
 // false
 // compare(100, 100)
 // true
 // compare(100, '100')
 // false
+//=========================================================
 
 
 EXERCISE 7:
 
 // create a function called isEven which takes 1 arg: a,  and return true if the argument passed is even and false if is odd.
 
-// example call 
+//======================  EXAMPLE  ========================
 // isEven(7)
 // false
 // isEven(2)
 // true
-
-
-
-
-
-
+//=========================================================
 
 
 EXERCISE 8:
 
-//knowing that the minimum age for driving a 50cc scooter is 15 define a variable called age
+// knowing that the minimum age for driving a 50cc scooter is 15 define a variable called age
 // and then return true if you are old enough to drive a scooter and false if you are not
 
-
-
-
-
-
+//======================  EXAMPLE  ========================
+// checkAge(21, 15)
+// true
+// checkAge(12,15)
+// false
+// checkAge(100, 60)
+// true
+//=========================================================
 
 
 EXERCISE 9:
 
-//define the name of the user
-//define the year of birth of the user
-//return his name and current age in a sentence like in the example:
-// ******EXAMPLE******
-//Hello Jason, you are 34 years old
+// define the name of the user
+// define the year of birth of the user
+// return his name and current age in a sentence like in the example:
 
-
-
-
-
-
-
+//======================  EXAMPLE  ========================
+// getAge(1982, 2018, 'Antonello')
+// `Hello Antonello you are 36 years old`
+// getAge(1960, 1970, 'Mike')
+// `Hello Mike you are 10 years old`
+// getAge(1200, 2000, 'Ottaviano')
+// `Hello Ottaviano you are 800 years old`
+//=========================================================
 
 
 EXERCISE 10:
 
-//define the user year of birth
-//assign the current year to a variable.
-//then return the following message, replacing the word *'DAYS'* with the actual value
+// define the user year of birth
+// assign the current year to a variable.
+// then return the following message, replacing the word *'DAYS'* with the actual value
 
-// you have lived for *'DAYS'* days already!
-
-
-
-
-
-
+//======================  EXAMPLE  ========================
+// howManyDays(2016, 2018)
+// `you have lived for 730 days already!`
+// howManyDays(1982, 2018)
+// `you have lived for 13140 days already!`
+//=========================================================
 
 
 EXERCISE 11:
 
-//It's hot out! Let's make a converter based on the steps here:
-http://www.mathsisfun.com/temperature-conversion.html
+// It's hot out! Let's make a converter based on the steps here:
+// http://www.mathsisfun.com/temperature-conversion.html
 
 // Store a celsius temperature into a variable.
 // Convert it to fahrenheit and output the result.
 // Now store a fahrenheit temperature into a variable.
 // Convert it to celsius and output the result.
 
-
+//======================  EXAMPLE  ========================
+// toCelsius(102)
+// 39
+// toFahr(39)
+// 102
+// toCelsius(68)
+// 20
+// toFahr(57)\
+// 135
+//=========================================================
 
 
 
