4f93639bf037e668d168589241a6489d863068a5
GK <george@barcelonacodeschool.com>
2020-06-30 12:03:15 +0200

    added b6e17, b2e15 with tests

---
+++ test/b2exercise15.test.js
@@ -0,0 +1,20 @@
+var assert                  = require('chai').assert
+var longestString             = require('../02_javascript/block2/exercise15').longestString
+
+
+
+describe("#test1", function(){
+    it ('longestString should be an function', function(){
+        assert.typeOf(longestString, 'function')
+    })
+})
+describe("#test2", function(){
+    it ("longestString should be watermelon", function(){
+        assert.equal(longestString(['banana','kiwi','orange','apple','watermelon','pear'],5), 'watermelon')
+    })
+})
+describe("#test3", function(){
+    it ("longestString should be stefano", function(){
+        assert.equal(longestString(['antonello','stefano','george'],6), 'stefano')
+    })
+})
