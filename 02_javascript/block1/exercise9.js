d53a27459692d68c7452c2ef0a2876868a89c2ad
Shannon Mettry <shannonmettry12@gmail.com>
2021-10-08 09:05:04 +0000

    Update exercise9.js

--- 02_javascript/block1/exercise9.js
+++ 02_javascript/block1/exercise9.js
@@ -1,5 +1,7 @@
     var getAge  = function (birth_year, now, name) {
-        return 
+        return now - birth_year;
     }
+    
+    console.log(getAge(1990, 2021, 'Shannon'));
 
     module.exports = {getAge}
