9696aa276ec522256ef350760c636b5c3ae90f95
Shannon Mettry <shannonmettry12@gmail.com>
2021-10-08 08:24:11 +0000

    Update exercise3.js

--- 02_javascript/block1/exercise3.js
+++ 02_javascript/block1/exercise3.js
@@ -1,10 +1,7 @@
 var ageCalc = function(date_of_birth, future_year){
+return future_year - date_of_birth;}
 
-	// your code goes here 
-
-	// and return something :)
-    return 
-}
+console.log(ageCalc(1990, 2021));
 
 module.exports = {
     ageCalc
