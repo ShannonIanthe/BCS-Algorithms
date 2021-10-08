257235c9f4fbe9062b6925f59928ad5f5310d464
Shannon Mettry <shannonmettry12@gmail.com>
2021-10-08 08:25:41 +0000

    Update exercise4.js

--- 02_javascript/block1/exercise4.js
+++ 02_javascript/block1/exercise4.js
@@ -1,6 +1,7 @@
 var howManyTeas = function(age, end_age, teas_day){
-    return 
+    return  ((end_age - age) * teas_day);
 }
+console.log(howManyTeas(18, 60,2));
 
 module.exports = {
     howManyTeas
