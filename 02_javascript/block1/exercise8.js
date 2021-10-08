f95c2ef90861b87093c4c4b04d83c3eaca7628aa
Shannon Mettry <shannonmettry12@gmail.com>
2021-10-08 09:03:02 +0000

    Update exercise8.js

--- 02_javascript/block1/exercise8.js
+++ 02_javascript/block1/exercise8.js
@@ -1,8 +1,15 @@
 
-
 var checkAge = function (age, minAge){
-    return 
+return function(age) {
+    return age > minAge; 
+    }
 }
+
+console.log(checkAge(18,22))
+
+
+
+//Still working on this one //
 
 module.exports = {
     checkAge
