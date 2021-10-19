f34f437ddfaefc877780ba53df8252a66bc4c7ca
ShannonIanthe <shannonmettry12@gmail.com>
2021-10-19 15:07:20 +0200

    Almost finished with Block3 ex9

--- 02_javascript/block3/exercise8.js
+++ 02_javascript/block3/exercise8.js
@@ -1,4 +1,19 @@
 
+// write a function called timesTwo which takes an array as an argument.
+// inside it use the forEach loop and push the numbers of the array to a 
+// new array and multiply them by two on the way then return the new array
+
+function timesTwo(arr){
+    var myArray= [] // empty array to push the numbers to the new array
+    arr.forEach(element => {
+        myArray.push(element * 2)
+    });
+    return myArray
+}
+//======================  EXAMPLE  ========================
+timesTwo([10,10,10])
+
+
 module.exports ={
     timesTwo
 }
--- 02_javascript/block3/exercise9.js
+++ 02_javascript/block3/exercise9.js
@@ -1,4 +1,18 @@
+// write function called twoArrays which takes two arrays as arguments, 
+// these arrays are of the same length
+// inside it declare a variable called count
+// it then loops and compare each element of one array with that of the other, 
+// in pairs with same indexes and every time it finds a match it increases the 
+// number of count by one then return count
 
+function twoArrays(arr1, arr2){
+    var count =0;
+    arr1.forEach(element,index => {
+        if (element === arr2[index])
+        count ++;      
+    });
+    return twoArrays
+}
 
 module.exports ={
     twoArrays
