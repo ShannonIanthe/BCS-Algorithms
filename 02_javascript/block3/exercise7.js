b080a839f8f01f6eb7a1c7eeecfb6602d00d2591
Shannon Mettry <shannonmettry12@gmail.com>
2022-06-26 00:29:49 +0200

    block 3 exercise 10

--- 02_javascript/block3/exercise7.js
+++ 02_javascript/block3/exercise7.js
@@ -1,15 +1,13 @@
 // write a function called multy which takes an array as argument and multiply 
 // each number and then returns the result.
 
-function multy(arr){
-	var multy = 1;
+function multy(arr) {
+	mult = 1;
 	arr.forEach(element => {
-		multy = multy * element;
+			mult =  element * mult;
 	});
-	return multy;
+	return mult;
 }
-
-multy([10,10,10])
 
 
 
--- 02_javascript/block3/exercise8.js
+++ 02_javascript/block3/exercise8.js
@@ -1,17 +1,11 @@
 
-// write a function called timesTwo which takes an array as an argument.
-// inside it use the forEach loop and push the numbers of the array to a 
-// new array and multiply them by two on the way then return the new array
-
-function timesTwo(arr){
-    var myArray= [] // empty array to push the numbers to the new array
+function timesTwo(arr) {
+    var newArr = [];
     arr.forEach(element => {
-        myArray.push(element * 2)
+        newArr.push(element * 2)
     });
-    return myArray
+    return newArr;
 }
-//======================  EXAMPLE  ========================
-timesTwo([10,10,10])
 
 
 module.exports ={
