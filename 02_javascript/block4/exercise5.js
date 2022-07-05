e7e87ba6bd870439b3d0db34b3e827404dd72481
Shannon Mettry <shannonmettry12@gmail.com>
2022-07-05 23:16:44 +0200

    still working on block4 ex6

--- 02_javascript/block4/exercise5.js
+++ 02_javascript/block4/exercise5.js
@@ -1,15 +1,18 @@
 function howManyCaps(str) {
-    var caps = 0;
-    var whichCaps = [];
-    str.forEach((element, index) => {
-        if (element === str.Uppercase(index)) {
-            caps++;
+    let count = 0;
+    let whichCap = [];
+    let capWords = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    for (var i = 0; i < str.length; i++) {
+        for (var j = 0; j < str[i].length; j++) {
+            if (capWords.includes(str[i][j])) {
+                count++;
+            }
         }
-        
-    }); return `There are ${caps} capitals and these are `;
+    }
+    return `There are ${count} capitals and these are ${whichCap}`
 }
-
-
 module.exports = {
     howManyCaps
 }
+
+// not sure how to display which letters are capitalized
--- 02_javascript/block4/exercise6.js
+++ 02_javascript/block4/exercise6.js
@@ -1,3 +1,13 @@
+function numberConverter(arr) {
+	let number = 1;
+	const numbers = []
+	arr.forEach(element => {
+		element * number;
+		arr.push(numbers);
+		
+	});
+return numbers;
+}
 
 
 module.exports ={
