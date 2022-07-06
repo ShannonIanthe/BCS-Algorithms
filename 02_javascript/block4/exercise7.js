8890324f35820f2553ce70c843f5df96d59b12fb
Shannon Mettry <shannonmettry12@gmail.com>
2022-07-06 22:29:15 +0200

    block 4 ex7

--- 02_javascript/block4/exercise7.js
+++ 02_javascript/block4/exercise7.js
@@ -1,4 +1,11 @@
-
+function booleanChecker(arr, maxCapacity) {
+	let bool = [];
+	arr.forEach(element => {
+		if (typeof element == "boolean") {
+			bool++;
+		}	
+	}); return `${bool} booleans were found`
+}
 
 module.exports = {
 	booleanChecker
